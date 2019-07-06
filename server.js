fa688d7a7f34e42ab30a43c3f067617f4f24d91d
Miguel Martinez <miguel.martinez11@gmail.com>
2019-07-06 21:39:13 +0200

    heroku post-install

--- server.js
+++ server.js
@@ -131,7 +131,14 @@ app.get('/api/convert', (req,res)=>{
     
 })
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static('client/build'))
 
+    const path=require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
